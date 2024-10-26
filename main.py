efc114f8a5997f96a32f3ff7e9505b3e5f0f5807
Ashutosh45-maker <ashutoshparadkar05@gmail.com>
2024-10-26 21:23:07 +0530

    added activity 2

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 #activity 1
-myarr=["Sachin","Dhoni","Virat","Rohit","DeVilliers"]
-# print(len(myarr))
-# print(myarr[-1])
-# print(myarr[0])
+# myarr=["Sachin","Dhoni","Virat","Rohit","DeVilliers"]
+# # print(len(myarr))
+# # print(myarr[-1])
+# # print(myarr[0])
 
-myarr.append("Kapil")
-print(myarr)
-myarr.remove("DeVilliers")
-myarr.sort()
-myarr.pop(0)
-myarr.reverse()
+# myarr.append("Kapil")
+# print(myarr)
+# myarr.remove("DeVilliers")
+# myarr.sort()
+# myarr.pop(0)
+# myarr.reverse()
 
 
-myarr=myarr[1:3]
-print(myarr)
+# myarr=myarr[1:3]
+# print(myarr)
+
+
+#activity 2
+
+mydict={1:"cricket",2:"Badminton",3:"swimminng",4:"tennis"}
+
+dict={"name":"Ashutosh","age":20}
+
+dict["address"]="mumbai"
+
+
+dict["age"]=72
+
+dict.pop("age")
+dict.clear()
+print(dict)
