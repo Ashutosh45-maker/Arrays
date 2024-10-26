f20dc06f698c822a52e18ba58d47f697e0ac0c55
Ashutosh45-maker <ashutoshparadkar05@gmail.com>
2024-10-26 21:03:27 +0530

    arrays

---
+++ main.py
@@ -0,0 +1,16 @@
+#activity 1
+myarr=["Sachin","Dhoni","Virat","Rohit","DeVilliers"]
+# print(len(myarr))
+# print(myarr[-1])
+# print(myarr[0])
+
+myarr.append("Kapil")
+print(myarr)
+myarr.remove("DeVilliers")
+myarr.sort()
+myarr.pop(0)
+myarr.reverse()
+
+
+myarr=myarr[1:3]
+print(myarr)
